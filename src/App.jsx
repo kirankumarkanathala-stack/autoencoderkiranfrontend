88f7eff458e1c697e320ca2fe0bd505751b3fbb3
kiran kumar <kanathalakirankumar@gmail.com>
2025-09-07 02:20:59 +0200

    private huggingspace proxy added

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ export default function AutoencoderApp() {
 
     try {
       const response = await axios.post(
-        "https://kiran6969-autoencoderkiranbackend1.hf.space/reconstruct",
+        "https://kiran6969-autoencoderkiranbackend1.hf.space/proxy/reconstruct",
         formData,
         {
           headers: { "Content-Type": "multipart/form-data" },
